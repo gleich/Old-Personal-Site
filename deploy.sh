8a257f53cecf3458860885239c0900347fe7463f
Matthew Gleich <matthewgleich@gmail.com>
2020-01-02 17:49:18 -0500

    Remove deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-{
-    git clean  -d  -fx .
-    git checkout gh-pages
-    git add .
-    git commit -m "Build Latest"
-    git push "https://${{ secrets.ghToken }}@github.com/Matt-Gleich/Matt-Gleich.github.io" || true
-} || {
-    echo "Nothing new"
-}
